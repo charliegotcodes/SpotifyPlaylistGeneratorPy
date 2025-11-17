57a89a792940449c7acd206564bc466bc5e69c52
kevin Charles <122123556+charliegotcodes@users.noreply.github.com>
2025-11-17 03:01:07 -0500

    hardcoded redirect uri in config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os, re
 
 CLIENT_ID     = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-REDIRECT_URI  = os.getenv("SPOTIPY_REDIRECT_URI")
+REDIRECT_URI  = "https://spotifyplaylistgeneratorpy.onrender.com/callback"
 
 AUTH_URL      = "https://accounts.spotify.com/authorize"
 TOKEN_URL     = "https://accounts.spotify.com/api/token"
